587663dedd06590715149aaf0ccce17b69c5740a
YuhBish <70145942+YuhBish@users.noreply.github.com>
2020-09-28 12:52:07 +0200

    Update övn.2.2.py
    La till kommentarer

--- övn.2.2.py
+++ övn.2.2.py
@@ -1,6 +1,6 @@
-import math
-r = float(input("Sfärens radie?"))
-v = 4* math.pi * r ** 3/3
-a = 4* math.pi * r ** 2
+import math # Importerade math modulen
+r = float(input("Sfärens radie? ")) # Frågar om radien och man ska svara med vad radien är
+v = 4* math.pi * r ** 3/3 # Räknar ut volymen
+a = 4* math.pi * r ** 2 # Räknar ut arean
 
-print ("Sfärens area är:", a, "Sfärens volym är:", v )
+print ("Sfärens area är:", a, "Sfärens volym är:", v ) # Skriver ut radien och arean
